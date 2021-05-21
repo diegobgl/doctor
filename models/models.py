46d26d479f795c5909f8d3dcbb6792557bd989a1
diego gajardo <diegobgajardo@gmail.com>
2021-05-20 21:16:11 -0400

    Update models.py

--- models/models.py
+++ models/models.py
@@ -5,5 +5,5 @@ from odoo import models, fields, api
 class doctor(models.Model):
       _name = 'doctor.doctor'
 
-       nombre_doctor = fields.Char(string="Nombre doctor")
-       especialidad = fields.Char(string="Especialidad")
+       nombre_doctor = fields.Char(string='Nombre doctor')
+       especialidad = fields.Char(string='Especialidad')
